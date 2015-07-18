b8d5daef1bb5b42d1160793dd99e890ec724d00a
Richard Saethang <richard.saethang@gmail.com>
2015-07-18 11:01:07 -0700

    fix python syntax formatting

--- 008_largest_product_in_series.py
+++ 008_largest_product_in_series.py
@@ -1,62 +1,80 @@
 # Problem 8 - Largest product in a series
 # Find the thirteen adjacent digits in the 1000-digit number
 # that have the greatest product. What is the value of this product?
-# 
 # NOTE: first program written in Python - Be gentle ^__^
 
-# used python instead of node because 
+# used python instead of node because
 # JavaScript only supports 53 bit integers
 # http://www.2ality.com/2012/07/large-integers.html
 
-# set 1000-digit number in longNumber var 
-longNumber = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+# set 1000-digit number in longNumber var
+longNumber = 73167176531330624919225119674426574742355
+349194934969835203127745063262395783180169848018694788
+518438586156078911294949545950173795833195285320880551
+112540698747158523863050715693290963295227443043557668
+966489504452445231617318564030987111217223831136222989
+342338030813533627661428280644448664523874930358907296
+290491560440772390713810515859307960866701724271218839
+987979087922749219016997208880937766572733300105336788
+122023542180975125454059475224352584907711670556013604
+839586446706324415722155397536978179778461740649551492
+908625693219784686224828397224137565705605749026140797
+296865241453510047482166370484403199890008895243450658
+541227588666881164271714799244429282308634656748139191
+231628245861786645835912456652947654568284891288314260
+769004224219022671055626321111109370544217506941658960
+408071984038509624554443629812309878799272442849091888
+458015616609791913387549920052406368991256071760605886
+116467109405077541002256983155200055935729725716362695
+61882670428252483600823257530420752963450
+
 
 # split the longNumber var into an array of single digits
 digitArray = map(int, str(longNumber))
 # set the limit of numbers to multiply at one time
 limit = 13
 
+
 # find the products of a series by passing an array of factors
-def findProductsOfSeries(_factorsArray):
+def find_products_of_series(_factors_array):
     # set answer to default 1
     answer = 1
     # loop through the factors array
-    for x in range(0, len(_factorsArray)):
+    for x in range(0, len(_factors_array)):
         # multiply each number to the previous product until array is empty
         # make sure answers are converted from string to int before multiplying
-        answer *= int(_factorsArray[x])
-    #return the product
+        answer *= int(_factors_array[x])
+    # return the product
     return answer
+
 
 # traverse through the long number 13 digits at a time
 # return highest product
-def findThirteenDigits(_num):
+def find_thirteen_digits(_num):
     # set rangeEnd to passed limit
-    rangeEnd = _num
+    range_end = _num
     # set default highestProduct var
-    highestProduct = 0
+    highest_product = 0
     # loop through longNumber, 13 digits at a time
-    for x in range(0, (len(digitArray)-(_num-1))):
+    for x in range(0, (len(digitArray) - (_num - 1))):
         # set/reset default array that factors get stored
-        factorArray = []
+        factor_array = []
         # loop through the 13 digit sample
-        for j in range(x, rangeEnd):
+        for j in range(x, range_end):
             # push the digits into an array
-            factorArray.append(digitArray[j])
+            factor_array.append(digitArray[j])
         # set the nextProduct var to the value of the next 13 digit product
-        nextProduct = findProductsOfSeries(factorArray)
+        next_product = find_products_of_series(factor_array)
         # compare if the current highestProduct is less than the new product
-        if highestProduct < nextProduct:
+        if highest_product < next_product:
             # if so, set the nextProduct as the highestProduct
-            highestProduct = nextProduct
+            highest_product = next_product
 
         # increase range by 1 to grab the next 13 in range
-        rangeEnd += 1
+        range_end += 1
 
     # after looping through all 1000 digits, return the highestProduct
-    return highestProduct
+    return highest_product
 
 
-
-print "Exercise 8 - Largest product in series is: %d" % findThirteenDigits(limit)
-
+print "Exercise 8 - Largest product in series is:", find_thirteen_digits(limit)
--- 009_special_pythagorean_triplet.py
+++ 009_special_pythagorean_triplet.py
@@ -2,23 +2,28 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-import math   # This will import math module, for math.floor
+import math  # This will import math module, for math.floor
 
-def findNumbersEqualThousand(_limit):
-    #this need a better range but assumed number can't be larger than totalNum/3
+
+def find_numbers_equal_thousand(_limit):
+    # this need a better range but assumed number
+    # can't be larger than totalNum/3
     # or else it will be greater than b or c
-    rangeA = int(math.floor(_limit/3))
-    rangeB = int(math.floor(_limit/2))
-    for a in range(0, rangeA): 
-        # in current loop, b can't be larger than _limit/2, or won't add up
-        for b in range(a+1, rangeB):
-            # in current loop, b can't be larger than 1000-(b-a), or won't add up
-            for c in range(b+1, _limit-(b-a)):
-                #if asquared + bsquared = csquared AND a+b+c=1000, then tell me what it is
-                if ((a*a) + (b*b) == (c*c) and (a+b+c) == _limit):
-                    print "a: %d || b: %d || c: %d" % (a,b,c)
-                    #return product of abc
-                    return a*b*c
+    range_a = int(math.floor(_limit / 3))
+    range_b = int(math.floor(_limit / 2))
+    for a in range(0, range_a):
+        # in current loop, b can't be larger than _limit/2
+        # or won't add up
+        for b in range(a + 1, range_b):
+            # in current loop, b can't be larger than 1000-(b-a)
+            # or won't add up
+            for c in range(b + 1, _limit - (b - a)):
+                # if asquared + bsquared = csquared AND a+b+c=1000
+                # then tell me what it is
+                if ((a * a) + (b * b) == (c * c) and (a + b + c) == _limit):
+                    print "a: %d || b: %d || c: %d" % (a, b, c)
+                    # return product of abc
+                    return a * b * c
 
-#print answer
-print "Exercise 9 - product of abc is: %d" % findNumbersEqualThousand(1000)
+# print answer
+print "Exercise 9 - product of abc is: %d" % find_numbers_equal_thousand(1000)
